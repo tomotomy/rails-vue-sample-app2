e8eb000a86f972e6fefc50744ae885726e112fc9
tomotomy <tomo1999724@gmail.com>
2019-09-20 16:11:25 +0900

    show employees on home

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # APIコントローラへのルーティング
+  root to: 'home#index'
+
   namespace :api, {format: 'json'} do
     namespace :v1 do
       resources :employees, only: [:index, :show]
